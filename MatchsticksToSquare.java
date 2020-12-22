b14bc8ed376933fe5d162f545a225e879f00ecd9
Dhruv <jawalkad@oregonstate.edu>
2020-12-21 20:21:33 -0800

    update matchsticks

--- MatchsticksToSquare.java
+++ MatchsticksToSquare.java
@@ -1,33 +1,35 @@
-// Time Complexity : O(4^n/4), n is length of nums
-// Space Complexity : O(n), Integer[], O(n/4) recursion call stack 
+// Time Complexity : O(4^n), n is length of nums
+// Space Complexity : O(n), worst case recursion call stack 
 // Did this code successfully run on Leetcode : yes
 
 // Your code here along with comments explaining your approach
 // bruteforce is try adding nums[i] to either of the 4 groups and check if possible to form square, sum of groups is equal at the end
-// recursively this is 4^n, we could sort the array and stop when groupSums[i] reaches overallSum/4, hence reducing complexity to 4^n/4
+// recursively this is 4^n, we could sort the array and stop when groupSums[i] reaches overallSum/4
 
 class Solution {
-    long overallSum = 0;
     
     public boolean makesquare(int[] nums) {
         if(nums==null || nums.length<4)
             return false;
         
-        int cur = 0;
-        long[] groupSums = new long[4];
-        Integer[] sortedNums = new Integer[nums.length];
-        
+        long sum = 0;
         for(int i=0; i<nums.length; i++){
-            overallSum += nums[i];
-            sortedNums[i] = nums[i];
+            sum += nums[i];
         }
         
-        Arrays.sort(sortedNums, (a,b) -> b.compareTo(a));      
-        return dfs(cur, sortedNums, groupSums);
+        if(sum%4 != 0)
+            return false;
+        
+        int cur = nums.length-1;
+        long[] groupSums = new long[4];
+        long target = sum/4;
+        
+        Arrays.sort(nums);      
+        return dfs(cur, nums, groupSums, target);
     }
     
-    private boolean dfs(int cur, Integer[] nums, long[] groupSums){
-        if(cur==nums.length){
+    private boolean dfs(int cur, int[] nums, long[] groupSums, long target){
+        if(cur==-1){
             for(int i=1; i<groupSums.length; i++){
                 if(groupSums[i]!=groupSums[i-1]){
                     return false;
@@ -36,16 +38,16 @@ class Solution {
             return true;
         }
         
-        boolean isPossible = false;
-        
         for(int i=0; i<groupSums.length; i++){
+            if(groupSums[i]+nums[cur]>target)
+                continue;
+            
             groupSums[i] += nums[cur]; 
-            if(groupSums[i]<=overallSum/4){
-                isPossible = isPossible || dfs(cur+1, nums, groupSums);    
-            }
+            if(dfs(cur-1, nums, groupSums, target))
+                return true;
             groupSums[i] -= nums[cur];            
         }
         
-        return isPossible;
+        return false;
     }
 }
